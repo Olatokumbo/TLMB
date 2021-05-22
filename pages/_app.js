7b220afb2b9135fcefe6c7b6518985a2ec2b7944
David Faith <faithodesola@gmail.com>
2021-05-22 12:19:13 +0100

    Edited the Title Header

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head lang="en">
-        <title>TLMB</title>
+        <title>TLMB | Test out your Math Skills</title>
         <meta
           name="Description"
           content="TLMB is a simple fun quiz game that tests out your math skils. Try iy out"
--- pages/api/questions.js
+++ pages/api/questions.js
@@ -2,8 +2,6 @@ import fs from "fs";
 import path from "path";
 
 export default (req, res) => {
-  // res.status(200).json({ name: 'John Doe' })
-
   if (req.method === "GET") {
     const level = req.query.level?.toLowerCase();
     const dataPath = path.join(process.cwd(), 'questions', level+'.json');
